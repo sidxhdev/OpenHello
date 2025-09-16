117bb7a75742a9ca4191795c532f2aeae326d74a
Pratik Abaji Gangurde <162554404+PRATIKABAJIGANGURDE@users.noreply.github.com>
2025-09-16 11:12:55 +0530

    Made my hello card

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Pratik Gangurde",
+    photoUrl: "https://avatars.githubusercontent.com/u/162554404?v=4",
+    githubUrl: "https://github.com/PRATIKABAJIGANGURDE",
+  },
   
  
   // Add more contributors here
