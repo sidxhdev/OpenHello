411d2c4244a9e28ae287e1b67589111303fcf292
codewizard-26 <yashwantsinghhustles@gmail.com>
2025-09-17 13:09:29 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name : "Yashwant Singh",
+    photourl:"https://avatars.githubusercontent.com/u/206307438?v=4"
+    githubUrl: "https://github.com/codewizard-26",
+  },
   
  
   // Add more contributors here
