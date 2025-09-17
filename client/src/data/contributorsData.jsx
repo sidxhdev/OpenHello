5ffbf5ee6c5745697ebbd9fe6f15009661d01ec1
Sail Yeram <119130716+sailyeram@users.noreply.github.com>
2025-09-17 13:09:36 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,12 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+
+  {
+    name: "Sail Yeram",
+    photoUrl: "https://avatars.githubusercontent.com/u/119130716?v=4",
+    githubUrl: "https://github.com/sailyeram",
+  },
   
  
   // Add more contributors here
