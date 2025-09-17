109008bab7a47432ba01f99f7a9422565ae196b7
vedant-lab1 <manwarvedant5@gmail.com>
2025-09-17 13:09:35 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,8 +1,8 @@
 export const contributorsData = [
   {
-    name: "Siddesh Navthale",
-    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
-    githubUrl: "https://github.com/sidxhdev",
+    name: "Vedant Manwar",
+    photoUrl: "https://avatars.githubusercontent.com/u/220670347?s=400&v=4",
+    githubUrl: "https://github.com/vedant-lab1",
   },
   
  
