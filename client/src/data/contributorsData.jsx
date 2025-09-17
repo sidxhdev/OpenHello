b30d6567bace3fb04a8aa51438a4a51e4e120a99
Satnam109 <satnamsm2@gmail.com>
2025-09-17 13:09:28 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -5,6 +5,10 @@ export const contributorsData = [
     githubUrl: "https://github.com/sidxhdev",
   },
   
- 
+ {
+    name: "Satnam Singh",
+    photoUrl: "https://avatars.githubusercontent.com/u/220943082?s=400&u=5fff0745328b905215779b46b4582f2778d1015f&v=4",
+    githubUrl: "https://github.com/Satnam109",
+  },
   // Add more contributors here
 ];
