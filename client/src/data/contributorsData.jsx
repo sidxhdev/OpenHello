56684f943218d1756d48409d61a685a242d0c690
Sanika <sanikajitendras1512@gmail.com>
2025-09-17 13:09:32 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Sanika",
+    photoUrl: "https://github.com/account",
+    githubUrl: "https://github.com/SanikaJR1512",
+  },
   
  
   // Add more contributors here
