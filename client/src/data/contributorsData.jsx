e686c2c716cda45cae9fdaac46e651ba92d9182a
Aryan <aryanrane1544@gmail.com>
2025-09-17 13:03:31 +0530

    Added Changes

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,7 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
-  
+  {
+    name: "Aryan RAne",
+    photoUrl: "https://avatars.githubusercontent.com/u/232715208?s=400&v=4",
+    githubUrl: "https://github.com/aryan15-cmd",
+  },
  
   // Add more contributors here
 ];
