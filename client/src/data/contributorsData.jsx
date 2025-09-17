cb6c290f5035ff255265ea883f6ded4e2ef6f33c
Siddhi <siddhilagad3301@gmail.com>
2025-09-17 13:09:16 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,5 +1,11 @@
 export const contributorsData = [
   {
+    name: "Siddhi Lagad",
+    photoUrl: "https://avatars.githubusercontent.com/u/182053157?v=4",
+    githubUrl: "https://github.com/Siddhi-i",
+  },
+  
+  {
     name: "Siddesh Navthale",
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
