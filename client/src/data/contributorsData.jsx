7809aa20a00226dd4b16fd1edf26a6a66937665a
Samreen Bano <sameeen.2005@gmail.com>
2025-09-17 13:09:33 +0530

    Add Samreen Bano to contributors data

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+   {
+    name: "Samreen bano",
+    photoUrl: "https://avatars.githubusercontent.com/u/220556935?v=4",
+    githubUrl: "https://github.com/samreenbano22",
+  },
   
  
   // Add more contributors here
