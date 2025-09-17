d136c3759ecfec8a7c663e856d7df485a2ead5ed
kadambari1906 <kadambaritupkar60@gmail.com>
2025-09-17 13:14:38 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://github.com/account",
     githubUrl: "https://github.com/kadambari1906",
   },
+  {
+    name: "kadambari tupkar",
+    photoUrl: "https://avatars.githubusercontent.com/u/219292207?s=400&v=4",
+    githubUrl: "https://github.com/kadambari1906",
+  },
   
  
   // Add more contributors here
