d8f221aaa28a8fc25ce1b04599dc66d31064df32
Diya Patel <diya070405@gmail.com>
2025-09-17 13:04:29 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -4,6 +4,11 @@ export const contributorsData = [
     photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
     githubUrl: "https://github.com/sidxhdev",
   },
+  {
+    name: "Diya Patel",
+    photoUrl: "https://avatars.githubusercontent.com/u/182648360?v=4&size=64",
+    githubUrl: "https://github.com/diya070405",
+  },
   
  
   // Add more contributors here
